d49a8193c0dcf96401fcdbe15280c92097529208
thkim1128 <ys-thkim@hanmail.net>
2017-10-11 00:27:27 +0900

    STK

---
+++ 1.jl
@@ -0,0 +1,17 @@
+#사진 올리기
+#===============================
+git add .
+git commit -am "Initial Commit"
+git push origin master
+fatal: 'origin' does not appear to be a git repository
+fatal: Could not read from remote repository.
+
+Please make sure you have the correct access rights
+and the repository exists.
+git init
+Reinitialized existing Git repository in C:/Users/ys-th/Desktop/Julia/HW_Astdynmc/.git/
+git remote add origin https://github.com/thkim1128/RocketMania
+git add .
+git commit -am "Initial Commit"
+git push origin master
+#=================================
